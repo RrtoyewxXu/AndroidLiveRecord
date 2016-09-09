a42a3c7c898101bf76c631093e04d6bc846b597c
RrtoyewxXu <rrtoyewx@gmail.com>
2016-09-09 19:05:02 +0800

    MediaMuxer and mediaExactor finish

---
+++ mediaexactorandmediamuxer/src/main/java/com/rrtoyewx/mediaexactorandmediamuxer/main.java
@@ -0,0 +1,87 @@
+package com.rrtoyewx.mediaexactorandmediamuxer;
+
+import android.app.Activity;
+import android.media.MediaCodec;
+import android.media.MediaExtractor;
+import android.media.MediaFormat;
+import android.media.MediaMuxer;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class main extends Activity {
+
+    private static final String SDCARD_PATH = Environment.getExternalStorageDirectory().getPath();
+
+    private MediaExtractor mMediaExtractor;
+    private MediaMuxer mMediaMuxer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    process();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    protected boolean process() throws IOException {
+
+        mMediaExtractor = new MediaExtractor();
+        mMediaExtractor.setDataSource(SDCARD_PATH + "/input.mp4");
+
+        int mVideoTrackIndex = -1;
+        int framerate = 0;
+        for (int i = 0; i < mMediaExtractor.getTrackCount(); i++) {
+            MediaFormat format = mMediaExtractor.getTrackFormat(i);
+            String mime = format.getString(MediaFormat.KEY_MIME);
+            if (!mime.startsWith("video/")) {
+                continue;
+            }
+            framerate = format.getInteger(MediaFormat.KEY_FRAME_RATE);
+            mMediaExtractor.selectTrack(i);
+            mMediaMuxer = new MediaMuxer(SDCARD_PATH + "/ouput.mp4", MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+            mVideoTrackIndex = mMediaMuxer.addTrack(format);
+            mMediaMuxer.start();
+        }
+
+        if (mMediaMuxer == null) {
+            return false;
+        }
+
+        MediaCodec.BufferInfo info = new MediaCodec.BufferInfo();
+        info.presentationTimeUs = 0;
+        ByteBuffer buffer = ByteBuffer.allocate(500 * 1024);
+        while (true) {
+            int sampleSize = mMediaExtractor.readSampleData(buffer, 0);
+            if (sampleSize < 0) {
+                break;
+            }
+            mMediaExtractor.advance();
+            info.offset = 0;
+            info.size = sampleSize;
+            info.flags = MediaCodec.BUFFER_FLAG_SYNC_FRAME;
+            info.presentationTimeUs += 1000 * 1000 / framerate;
+            mMediaMuxer.writeSampleData(mVideoTrackIndex, buffer, info);
+        }
+
+        mMediaExtractor.release();
+
+        mMediaMuxer.stop();
+        mMediaMuxer.release();
+
+        return true;
+    }
+
+
+}
